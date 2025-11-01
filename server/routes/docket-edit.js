778b257916d1f0806ce44214534c4d043a7e6733
specialdk <specialdk55@gmail.com>
2025-11-01 19:36:48 +0930

    Edit WBD build 2

--- server/routes/docket-edit.js
+++ server/routes/docket-edit.js
@@ -5,7 +5,7 @@
 
 const express = require("express");
 const router = express.Router();
-const pool = require("../config/database");
+const { pool } = require("../config/database");
 
 // ============================================
 // POST /api/dockets/edit
